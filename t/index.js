5a4946f0703cd1720cd1de76e0b426b45ed74200
offici5l <127665988+offici5l@users.noreply.github.com>
2024-11-27 20:18:22 +0100

    Update index.js

--- t/index.js
+++ t/index.js
@@ -48,7 +48,7 @@ export default {
         const jobData = await jobResponse.json();
         const job = jobData.jobs.find(job => job.name === JOB_NAME);
         if (job) {
-          return new Response(`\nJobName: ${job.name}\nJobStatus: ${job.status}\nJobConclusion: ${job.conclusion}\n\nSteps:\n\n` + job.steps.filter(step => ["validate", "download", "boot_img", "settings_apk", "upload"].includes(step.name)).map(step => `Name: ${step.name}\nStatus: ${step.status}\nConclusion: ${step.conclusion}\n`).join('\n'), { status: 200, headers: { 'Content-Type': 'text/plain' } });
+          return new Response(`\nJobName: ${job.name}\nJobStatus: ${job.status}\nJobConclusion: ${job.conclusion}\n\nSteps:\n\n` + job.steps.filter(step => ["validate", "download", "boot_img", "settings_apk", "upload"].includes(step.name)).map(step => `Name: ${step.name}\nStatus: ${step.status}\nConclusion: ${step.conclusion}\n\n`).join('\n'), { status: 200, headers: { 'Content-Type': 'text/plain' } });
         }
       }
       return new Response("Job not found", { status: 404 });
